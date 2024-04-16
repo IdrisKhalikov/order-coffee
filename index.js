06e12274c347874b4af0af76300604c053dd581d
mgmman <115487609+mgmman@users.noreply.github.com>
2024-04-16 18:09:06 +0500

    done 3-5

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+function pluralizeDrinks(count)
+{
+    const singleDigitRemainder = count % 10;
+    const twoDigitRemainder = count % 100;
+
+    if ((singleDigitRemainder === 0 || singleDigitRemainder >= 5)
+        || (twoDigitRemainder >= 11 && twoDigitRemainder < 20)) {
+        return 'Напитков';
+    }
+    if (singleDigitRemainder === 1) {
+        return 'Напиток';
+    }
+    return 'Напитка';
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const addButton = document.querySelector('.add-button');
     const form = document.querySelector('form');
@@ -9,4 +24,23 @@ document.addEventListener('DOMContentLoaded', function() {
         newFieldset.querySelector('.beverage-count').textContent = `Напиток №${drinkCount}`;
         form.insertBefore(newFieldset, addButton.parentNode);
     });
+    const modal = document.querySelector('.modal');
+    const modalContent = document.querySelector('.modal-content');
+    const closeButton = document.querySelector('.close-button');
+    const submitButton = document.querySelector('.submit-button');
+
+    submitButton.addEventListener('click', function() {
+        const countP = document.createElement('p');
+        countP.textContent = `Вы заказали ${drinkCount} ${pluralizeDrinks(drinkCount)}`;
+        modalContent.appendChild(countP);
+        modal.classList.add('active');
+    });
+
+    closeButton.addEventListener('click', function() {
+        modalContent.removeChild(modalContent.lastChild);
+        modal.classList.remove('active');
+    });
 });
+
+
+

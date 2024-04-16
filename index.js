4026d8bdf6db5d46b0a290cdc60799aa246326e1
mgmman <115487609+mgmman@users.noreply.github.com>
2024-04-16 18:54:20 +0500

    done 6

--- index.js
+++ index.js
@@ -37,12 +37,45 @@ document.addEventListener('DOMContentLoaded', function() {
 
     submitButton.addEventListener('click', function() {
         const countP = document.createElement('p');
-        countP.textContent = `Вы заказали ${drinkCount} ${pluralizeDrinks(drinkCount)}`;
+        countP.textContent = `Вы заказали ${formsCount} ${pluralizeDrinks(formsCount)}`;
         modalContent.appendChild(countP);
+        const fieldsets = document.querySelectorAll('.beverage');
+        const table = document.createElement('table');
+        const head = document.createElement('tr');
+        const drink = document.createElement('th');
+        drink.textContent = 'Напиток';
+        head.appendChild(drink);
+        const milk = document.createElement('th');
+        milk.textContent = 'Молоко';
+        head.appendChild(milk);
+        const misc = document.createElement('th');
+        misc.textContent = 'Дополнительно';
+        head.appendChild(misc);
+        table.appendChild(head);
+        fieldsets.forEach(fieldset => {
+            const selectedBeverage = fieldset.querySelector('select').value;
+            const selectedMilk = fieldset.querySelector('input[name="milk"]:checked').value;
+            const selectedOptions = Array.from(fieldset.querySelectorAll('input[name="options"]:checked'))
+                .map(option => option.nextElementSibling.innerText)
+                .join(', ');
+            const body = document.createElement('tr');
+            const drink1 = document.createElement('td');
+            drink1.textContent = selectedBeverage;
+            body.appendChild(drink1);
+            const milk1 = document.createElement('td');
+            milk1.textContent = selectedMilk;
+            body.appendChild(milk1);
+            const misc1 = document.createElement('td');
+            misc1.textContent = selectedOptions;
+            body.appendChild(misc1);
+            table.appendChild(body);
+        })
+        modalContent.appendChild(table);
         modal.classList.add('active');
     });
 
     closeButton.addEventListener('click', function() {
+        modalContent.removeChild(modalContent.lastChild);
         modalContent.removeChild(modalContent.lastChild);
         modal.classList.remove('active');
     });
